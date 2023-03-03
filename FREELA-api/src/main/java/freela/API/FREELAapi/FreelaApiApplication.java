d34675bb984129906d21c1a88d8b8d28df6f9b0b
GuilhermeFGoncalves <guilherme.goncalves@sptech.school>
2023-03-03 18:58:22 -0300

    inital

---
+++ FREELA-api/src/main/java/freela/API/FREELAapi/FreelaApiApplication.java
@@ -0,0 +1,13 @@
+package freela.API.FREELAapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FreelaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FreelaApiApplication.class, args);
+	}
+
+}
---
+++ FREELA-api/src/test/java/freela/API/FREELAapi/FreelaApiApplicationTests.java
@@ -0,0 +1,13 @@
+package freela.API.FREELAapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreelaApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
