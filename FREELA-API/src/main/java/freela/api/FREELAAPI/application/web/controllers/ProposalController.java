6cc04309522829c66a54f9cc8bfdc613e509b377
Guilherme Goncalves <guilhermegoncalves@NTB56.boxdelivery.local>
2023-04-04 19:08:15 -0300

    MUda funcao de proposta

--- FREELA-API/src/main/java/freela/api/FREELAAPI/application/web/controllers/ProposalController.java
+++ FREELA-API/src/main/java/freela/api/FREELAAPI/application/web/controllers/ProposalController.java
@@ -1,21 +1,34 @@
 package freela.api.FREELAAPI.application.web.controllers;
 
 import freela.api.FREELAAPI.domain.repositories.ProposalRepository;
+import freela.api.FREELAAPI.domain.repositories.UsersRepository;
 import freela.api.FREELAAPI.resourses.entities.Proposals;
+import freela.api.FREELAAPI.resourses.entities.Users;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/proposals")
 public class ProposalController {
     @Autowired
     private ProposalRepository proposalRepository;
+    
+    @Autowired
+    private UsersRepository usersRepository;
 
-    @PostMapping
-    public ResponseEntity<Proposals> post(@RequestBody Proposals proposals){
+    @PostMapping("/{userId}")
+    public ResponseEntity<Object> post(@PathVariable Integer userId,@RequestBody Proposals proposals){
+        Optional<Users> user = this.usersRepository.findById(userId);
+
+        if(!user.isPresent()){
+            return ResponseEntity.status(404).body("invalid user");
+        }
+        proposals.setOriginUser(user.get());
+        
         return ResponseEntity.status(201).body(this.proposalRepository.save(proposals));
     }
 
